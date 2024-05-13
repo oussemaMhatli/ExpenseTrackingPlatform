0ac3dc15197d859d69cb57c7e9ef976cb5d35c7e
samir267 <samirchemkhi09@gmail.com>
2024-05-13 22:14:34 +0100

    update

--- GestionFinanceFront/src/app/app-routing.module.ts
+++ GestionFinanceFront/src/app/app-routing.module.ts
@@ -8,10 +8,8 @@ import { GuestComponent } from './theme/layout/guest/guest.component';
 import DashAnalyticsComponent from './demo/dash-analytics copy/dash-analytics.component';
 import { TransactionComponent } from './demo/transaction/transaction.component';
 
-import { EditUserComponent } from './demo/user-management/components/edit-user/edit-user.component';
 import { linkGuardGuard } from './link-guard.guard';
 import { UserProfileComponent } from './demo/user-profile/user-profile.component';
-import { NgIf } from '@angular/common';
 
 import { TagsComponent } from './demo/tags/tags.component';
 
@@ -92,7 +90,7 @@ const routes: Routes = [
       },
       
       {path:"transaction",component:TransactionComponent,canActivate: [linkGuardGuard]},
-      {path:"UserProfile",component:UserProfileComponent,canActivate: [linkGuardGuard]}
+      {path:"UserProfile",component:UserProfileComponent,canActivate: [linkGuardGuard]},
       // {
       //   path: 'chart',
       //   loadComponent: () => import('./demo/chart & map/core-apex/core-apex.component')
@@ -109,7 +107,6 @@ const routes: Routes = [
       //   path: 'sample-page',
       //   loadComponent: () => import('./demo/sample-page/sample-page.component')
       // },
-      {path:"transaction",component:TransactionComponent},
       {path:"tags",component:TagsComponent}
 
 
