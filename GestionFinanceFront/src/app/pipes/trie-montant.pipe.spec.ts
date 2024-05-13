795e7771e8f268ace88d15a5a1f690b1afa973e2
ahmed stambouli <ahmed.stambouli98@gmail.com>
2024-05-13 19:48:41 +0100

    transaction filter

---
+++ GestionFinanceFront/src/app/pipes/trie-montant.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TrieMontantPipe } from './trie-montant.pipe';
+
+describe('TrieMontantPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TrieMontantPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
